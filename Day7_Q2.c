7d7f3d8ebcbad9e9ab1bde930f601044402867b3
Aryana Singh <aryanasingh143@gmail.com>
2026-08-20 23:19:26 +0530

    Day7_Q2 code

---
+++ Day7_Q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*Program to input a character and check whether it is a vowel or consonant using if–else*/
+int main()
+{
+    char ch;
+    printf("Enter a character:");
+    scanf("%c", &ch);
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+    {
+        printf("Vowel");
+    }
+    else
+    {
+        printf("Consonant");
+    }
+    return 0;
+}
