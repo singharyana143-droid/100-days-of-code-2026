5d9d864c342ec6fd2b25cdb85eacf26fa01bc9ab
Aryana Singh <aryanasingh43@gmail.com>
2026-08-20 22:38:16 +0530

    Day6_Q1 code

---
+++ Day6_Q1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*Program to input an integer and check whether it is even or odd using if–else.*/
+int main()
+{
+    int num;
+    printf("Enter an integer:");
+    scanf("%d", &num);
+    if (num % 2 == 0)
+    {
+        printf("%d is even.",num);
+    }
+    else
+   {
+    printf("%d is odd.",num);
+   }
+    return 0;
+}
